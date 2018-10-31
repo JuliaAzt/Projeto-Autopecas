f83d5ad7f59c74c007c186f70c9af66446cd45d5
Joaquim Júnior <jojuvi3@gmail.com>
2018-10-31 16:49:18 -0300

    sprint_1 pagina home quase pronta e referencia para outras pags

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,19 +2,25 @@
 <!DOCTYPE html>
 <html>
 
-<body>	
+<body >	
 <div class="fundo">
 		<div class="nav">
-				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/logoPrincipal.png " class="logo" >
+				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/logoPrincipal.png " class="logo" ></div>
 				
-					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<span class="paginas">Inicio</span>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<span class="paginas">Quem somos</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<span class="paginas">Onde Estamos?</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<span class="paginas">Nossos Contatos</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<span class="paginas">Nossa Filosofia</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<span class="paginas">Nossos Produtos</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<span class="paginas">Trabalhe conosco</span> &nbsp;&nbsp;
+				<div class="nounderline" ><a href = "<?php echo base_url('Home');?>">Início</a></div>
+				<div class="nounderline" ><a href = "<?php echo base_url('QuemSomos');?>">Quem somos</a></div>
+				<div class="nounderline" ><a href = "<?php echo base_url('OndeEstamos');?>">Onte estamos?</a></div>
+				<div class="nounderline" ><a href = "<?php echo base_url('OndeEstamos');?>">Nossos Contatos</a></div>
+				<div class="nounderline" ><a href = "<?php echo base_url('NossaFilosofia');?>">Nossa Filosofia</a></div>
+				<div class="nounderline" ><a href = "<?php echo base_url('NossosProdutos');?>">Nossos Produtos</a></div>
+				<div class="nounderline" ><a href = "<?php echo base_url('Trabalhe conosco');?>">Trabalhe conosco</a></div>
+					<!--<div><span class="paginas">Inicio</span></div>
+					<div><span class="paginas">Quem somos</span></div> 
+					<div><span class="paginas">Onde Estamos?</span></div> 
+					<div><span class="paginas">Nossos Contatos</span></div> 
+					<div><span class="paginas">Nossa Filosofia</span></div>
+					<div><span class="paginas">Nossos Produtos</span></div> 
+					<div><span class="paginas">Trabalhe conosco</span></div>-->
 					
 		</div>
 
@@ -27,80 +33,57 @@
 				<br>
 				<br>
 				<br>
-				<br>
-				<br>
-				<br>
-				<h1>TRABALHAMOS <br/>
+				<div> <h1>TRABALHAMOS <br/>
 				 COM AS MELHORES MARCAS</h1> 
 				<h2>Aqui na Neves Diniz Auto Peças, você encontra as peças</h2>
 				<h2>perfeitas para deixar seu carro perfeito</h2>
-				<h2>Venha nos fazer uma visita</h2>
+				<h2>Venha nos fazer uma visita</h2></div>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+
 		</div>
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
+
 		
-		<div >
-			<div>
-				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/bosch.png" id="bosch"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/3m.png" id="tresM"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/nakata.png" id="nakata"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			</div>
+		<div class="marcas">
+				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/bosch.png" id="bosch"> </div>
+				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/3m.png" id="tresM"></div>
+				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/nakata.png" id="nakata"></div>
 
 		</div>
+		
+</div>
+<div id="meioDaPagina">
+	<div id="textoMeioDaPagina">
+		Numa pesquisa feita com a população de Carandaí, a Neves Diniz Auto Peças levou o troféu Best of Business
+		<p id="auxMeioDaPagina">do Instituto Tiradentes como a melhor loja de autopeças da cidade.</p>
 
+	</div>
+</div>
+	<div class="finalDaPagina">
 
-		<div id="meioDaPagina">
-			<div id="textoMeioDaPagina">
-				Numa pesquisa feita com a população de Carandaí, a Neves Diniz Auto Peças levou o troféu Best of Business
-					<p id="auxMeioDaPagina">do Instituto Tiradentes como a melhor loja de autopeças da cidade.</p>
-
-			</div>
-			<br />
-		<br />
-		<br />
-	
-		<div class="finalDaPagina">
-			<div id ="atendimentoHome">
+			<div  class="gridImagens" id="atendimentoHome">
 				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/atendimentoHome.jpg" id ="imagemAtendimentoHome">
 				<div id="textoAtendimentoHome">Atendimento feito por profissionais capacitados</div>
 			</div>
-			
-			<div id = "pagamentoFacilitado">
+				
+			<div class="gridImagens" id="pagamentoFacilitado">
 				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/pagamentoHome.jpg" id = "imagemPagamentoFacilitado">
 				<div id="textoPagamentoFacilitado">Pagamento facilitado</div>
 			</div>
 
-			<div id = "solucaoProblema">
-				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/solucaoHome.jpg" id = "imagemSolucaoProblema">
-				<div id="textoSolucaoProblema">Encontre a solução para o seu problema</div>
+			<div  class="gridImagens" id="solucaoProblema">
+					<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/solucaoHome.jpg" id = "imagemSolucaoProblema">
+					<div id="textoSolucaoProblema">Encontre a solução para o seu problema</div>
 			</div>
-		<div>
 
-
-
-		</div>
-
-</div>
-
-		
-	
-
-
+	</div>
 </body>
 </html>
