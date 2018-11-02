5bb3afe638b53dfa7f8a5cfaae6be6ad5d92444d
Joaquim Júnior <jojuvi3@gmail.com>
2018-11-02 18:01:33 -0300

    Pagina OndeEstamos-Contatos pronta, consertei a divisao dos arquivos header e body da home

---
+++ application/views/Contatos.php
@@ -0,0 +1,45 @@
+<html>
+<body >
+<div class="Titulo">
+<h3 id="Contatos_t1">Entre em contato com a</h3>
+<div id="NevesDinizTexto"><h3> Neves Diniz Auto Peças</h3></div>
+</div>
+<div class="TextoDestacado">
+
+<h3>Oficinas mecânicas, taxistas, motoristas de Uber e outras empresas,</h3>
+<h3>venha fazer uma parceria conosco!</h3>
+</div>
+<div class="GridContatos">
+    <div class="Contatos">
+    <div><img src=" http://localhost/Projeto-Autopecas/assets/css/Imagens/call.png " id="imgContato" ></div>
+    <div id="topicoContato"><p>Telefone <br></p></div>
+    <p> 32 - 3331-5101</p>
+    <br><br>
+    <div id="topicoContato"><p>Whatsapp <br></p></div>
+    <p> 32 - 3331-5101</p>
+    </div>
+    <div class="Contatos">
+    <div><img src=" http://localhost/Projeto-Autopecas/assets/css/Imagens/email.png " id="imgContato" ></div>
+    <div id="topicoContato"><p>Email<br></p></div>
+    <p> realAutoPecas@gmail.com</p>
+    <br><br>
+    </div>
+    <div class="Contatos">
+    <div><img src=" http://localhost/Projeto-Autopecas/assets/css/Imagens/email.png " id="imgContato" ></div>
+    <div id="topicoContato"><p>Rua<br></p></div>
+    <p>Sete de Setembro</p>
+    <div id="topicoContato"><p>Bairro<br></p></div>
+    <p> Centro</p>
+    <div id="topicoContato"><p>Número<br></p></div>
+    <p> 337</p>
+
+    <div id="topicoContato"><p>Cidade<br></p></div>
+    <p> Carandaí</p>
+    </div>
+</div>
+
+</div>
+</body>
+</html>
+
+
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,9 +1,8 @@
 
 <!DOCTYPE html>
 <html>
+<body>
 
-<body >	
-<div class="fundo">
 		<div class="nav">
 				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/logoPrincipal.png " class="logo" ></div>
 				
@@ -25,65 +24,5 @@
 		</div>
 
 		
-		<div class = "escrita">
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<div> <h1>TRABALHAMOS <br/>
-				 COM AS MELHORES MARCAS</h1> 
-				<h2>Aqui na Neves Diniz Auto Peças, você encontra as peças</h2>
-				<h2>perfeitas para deixar seu carro perfeito</h2>
-				<h2>Venha nos fazer uma visita</h2></div>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
-				<br>
 
-		</div>
-
-		
-		<div class="marcas">
-				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/bosch.png" id="bosch"> </div>
-				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/3m.png" id="tresM"></div>
-				<div><img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/nakata.png" id="nakata"></div>
-
-		</div>
-		
-</div>
-<div id="meioDaPagina">
-	<div id="textoMeioDaPagina">
-		Numa pesquisa feita com a população de Carandaí, a Neves Diniz Auto Peças levou o troféu Best of Business
-		<p id="auxMeioDaPagina">do Instituto Tiradentes como a melhor loja de autopeças da cidade.</p>
-
-	</div>
-</div>
-	<div class="finalDaPagina">
-
-			<div  class="gridImagens" id="atendimentoHome">
-				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/atendimentoHome.jpg" id ="imagemAtendimentoHome">
-				<div id="textoAtendimentoHome">Atendimento feito por profissionais capacitados</div>
-			</div>
-				
-			<div class="gridImagens" id="pagamentoFacilitado">
-				<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/pagamentoHome.jpg" id = "imagemPagamentoFacilitado">
-				<div id="textoPagamentoFacilitado">Pagamento facilitado</div>
-			</div>
-
-			<div  class="gridImagens" id="solucaoProblema">
-					<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/solucaoHome.jpg" id = "imagemSolucaoProblema">
-					<div id="textoSolucaoProblema">Encontre a solução para o seu problema</div>
-			</div>
-
-	</div>
-</body>
 </html>
