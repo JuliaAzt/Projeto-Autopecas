2fa3e29503fb9148329d37262276bd35d4c2ce7b
Lucas Natali <lucasnatali16@gmail.com>
2018-11-07 16:39:42 -0200

    Projeto atualizado OK

---
+++ application/views/NossaFilosofia.php
@@ -0,0 +1,22 @@
+<html>
+<body >
+    <div class="TrabalheConoscoBackground">
+        
+        <div  id="TrabalheConoscoTitulo">
+            <h3>Nossa Filosofia</h3>
+            <h3 id="TrabalheConoscoTextoDestacado">Trabalhamos única e exclusivamente em função do nosso cliente.</h3>
+        </div>
+
+        <div id="TrabalheConoscoTextoPrincipal">
+            <p>Temos como regra sempre atender nosso cliente com a maior cordialidade possível e fazer tudo que estiver ao nosso alcance para satisfazê-lo.</p><br>
+
+            <p>É certo que nem sempre iremos conseguir, para isso queremos colocar nossos canais de comunicação à disposição daquele cliente que porventura tenha alguma critica a fazer. Entre em contato, não fique no anonimato, reclame conosco. <span id="trabalheConoscoSaibaMais">Clique aqui</span> e acesse nossa página de contatos e saiba como pode comunicar-se conosco. Seja qual for o tipo de problema estaremos sempre prontos a fazer o que for possível para bem atendê-los.</p><br>
+
+            <p>Nós não damos importância somente a sua compra, mas principalmente a você, isso porque não pretendemos vender somente uma vez, mas queremos conquistá-lo como nosso cliente amigo fiel. Nossa missão é sempre e cada vez mais atendê-lo melhor e com mais rapidez.</p><br>
+            
+            
+        </div>
+
+    </div>
+</body>
+</html>
