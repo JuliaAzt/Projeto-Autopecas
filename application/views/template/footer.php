4debdfda1bbb855bc0077d18672764b04f18bdeb
Joaquim Júnior <jojuvi3@gmail.com>
2018-11-21 14:28:04 -0200

    mudancas no footer e link de trabalhe conosco

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -4,8 +4,13 @@
 <footer>
 	
 	<div class="rodape">
-		<img src=" http://localhost/Projeto-Autopecas/assets/img/Imagens/redeSocial.png " class = "midias">
-		<span id="copyright">Copyright R</span>
+		<img src=" http://localhost/Projeto-Autopecas/assets/css/Imagens/redeSocial.png " class = "midias">
+		<span id="copyright">Copyright ®</span>
+		<br>
+		<div class="registro">
+		Desenvolvido por Voluta Soluções Digitais
+		</div>
+		
 	</div>
 </footer>
 </html>
